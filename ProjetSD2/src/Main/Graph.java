874ec24f095ec20a9373beba1e54304d481f55b8
Jehan de Foy <jehandefoy3@gmail.com>
2021-03-08 11:50:52 +0100

    add - Graph class which allows to create and manipulate graphs, Country class & .gitignore

---
+++ ProjetSD2/src/Main/Graph.java
@@ -0,0 +1,55 @@
+package Main;
+
+import java.io.File;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+
+public class Graph {
+
+	private Map<Country, Set<String>> outgoingRoads;
+	private Map<String, Country> cca3Country;	//References countries using their cca3
+
+	public Graph()  {
+		outgoingRoads = new HashMap<Country, Set<String>>();
+		cca3Country = new HashMap<String, Country>();
+	}
+
+	protected void addSummit(Country c) {
+		if(c == null) throw new IllegalArgumentException("Error: country can't be null");
+		outgoingRoads.put(c, new HashSet<String>());
+		cca3Country.put(c.getCca3(), c);
+	}
+
+	protected void addArch(Country c, String cca3) {
+		if(c == null) throw new IllegalArgumentException("Error: country can't be null");
+		if(cca3 == null) throw new IllegalArgumentException("Error: cca3 can't be null");
+		outgoingRoads.get(c).add(cca3);
+	}
+	
+	public Set<String> outgoingArches(Country c){
+		return outgoingRoads.get(c);
+	}
+
+	public boolean areAdjacent(Country c1, Country c2) {		
+		return outgoingRoads.get(c1).contains(c2.getCca3());
+	}
+	
+	public void calculerItineraireMinimisantNombreDeFrontieres(String cca3, String otherCca3, String file) {
+		//todo
+	}
+	
+	public void calculerItineraireMinimisantPopulationTotale(String cca3, String otherCca3, String file) {
+		//todo
+	}	
+
+}
---
+++ ProjetSD2/src/Main/SAXHandler.java
@@ -0,0 +1,46 @@
+package Main;
+
+import org.xml.sax.*;
+import org.xml.sax.helpers.DefaultHandler;
+
+public class SAXHandler extends DefaultHandler {
+	
+	private Country currentCountry; //the country SAX is actually reading
+	
+	Graph graph = new Graph();
+	
+	public Graph getGraph() {
+		return this.graph;
+	}
+
+	@Override
+	public void startDocument() throws SAXException {
+		// TODO Auto-generated method stub
+		super.startDocument();
+	}
+
+	@Override
+	public void endDocument() throws SAXException {
+		// TODO Auto-generated method stub
+		super.endDocument();
+	}
+
+	@Override
+	public void startElement(String uri, String localName, String qName, Attributes attributes) throws SAXException {
+		// TODO Auto-generated method stub
+		super.startElement(uri, localName, qName, attributes);
+	}
+
+	@Override
+	public void endElement(String uri, String localName, String qName) throws SAXException {
+		// TODO Auto-generated method stub
+		super.endElement(uri, localName, qName);
+	}
+
+	@Override
+	public void characters(char[] ch, int start, int length) throws SAXException {
+		// TODO Auto-generated method stub
+		super.characters(ch, start, length);
+	}
+
+}
